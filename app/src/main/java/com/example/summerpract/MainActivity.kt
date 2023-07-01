0a935e9266994199704a5bcea946153762779936
Ev1k <zhzh7237@gmail.com>
2023-07-01 13:30:49 +0300

    binding added

--- app/src/main/java/com/example/summerpract/MainActivity.kt
+++ app/src/main/java/com/example/summerpract/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.example.summerpract
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.example.summerpract.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val newBinding = ActivityMainBinding.inflate(layoutInflater)
+        binding = newBinding
+        setContentView(newBinding.root)
 
-        setContentView(R.layout.activity_main)
+        binding.button.setOnClickListener {
+            binding.button.text = "fffffffff"
+        }
+
+        binding.etLogin.addTextChangedListener {
+//            Log.e("etLogin", "Message: ${it?.toString()}")
+            if (!binding.etLogin.text.toString().isNullOrEmpty()) {
+                binding.tiLogin.error = null
+            } else {
+                binding.tiLogin.error = "ERROR"
+            }
+        }
 
 
     }

9c9b8bd0fb20b12082a56f901ad49c28d6070312
Ev1k <zhzh7237@gmail.com>
2023-07-02 13:56:54 +0300

    navigation

--- app/src/main/java/com/example/summerpract/MainActivity.kt
+++ app/src/main/java/com/example/summerpract/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.summerpract
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val controller =
+//            (supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment)
+//                .navController
     }
 }
